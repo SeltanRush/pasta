dfcf908dd6b0975b54738d9923010fe61a0972ff
Andrey Kuznetsov <seltanrush@gmail.com>
2020-06-23 23:26:22 +0700

    Added user controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async createUser(): Promise<void> {
+    console.log('createUser');
+  }
+
+  async findUser(): Promise<void> {
+    console.log('findUser');
+  }
+
+  async deleteUser(): Promise<void> {
+    console.log('deleteUser');
+  }
+
+  async updateUser(): Promise<void> {
+    console.log('updateUser');
+  }
+}
