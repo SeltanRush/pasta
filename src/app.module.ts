f9cccb5a43752ca279cdfa466003dae19883c224
Andrey Kuznetsov <seltanrush@gmail.com>
2020-06-24 17:21:28 +0700

    Created docker files. Setup docker env. Setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 import { UserModule } from './user/user.module';
 import { UserEntity } from './user/user.entity';
 
+const typeormConfig: TypeOrmModuleOptions = {
+  entities: [UserEntity],
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  database: process.env.PG_NAME,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  port: Number(process.env.PG_PORT),
+};
+
 @Module({
-  imports: [TypeOrmModule.forRoot({ entities: [UserEntity] }), UserModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), UserModule],
   controllers: [],
   providers: [],
 })
